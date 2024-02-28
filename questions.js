2fc6bcb69b3b8cfc877f9da98928e4c18128d8ba
Pratishtha36 <159642222+Pratishtha36@users.noreply.github.com>
2024-02-28 23:57:42 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,36 @@
+let questions= [
+  {  numb: 1,
+    question: "",
+    answer: "",
+    options:[
+
+    ]
+}
+
+
+
+{
+    numb:2,
+    question: "",
+    answer: "",
+    options:[
+
+    ]
+}
+{
+    numb:3,
+    question: "",
+    answer: "";
+    options:[
+
+    ]
+}
+{
+    numb: 4,
+    question: "";
+    answer: "";
+    options:[
+
+    ]
+}
+]
